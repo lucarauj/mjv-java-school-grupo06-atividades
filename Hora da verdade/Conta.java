a998c5edabe083cd64c62a4071b821b59f2bfb93
patricia-bairos <patricia.s.bairos@gmail.com>
2023-06-08 14:49:16 -0300

    feat/Desenvolver atributo agencia e metodo transferir valor

--- Hora da verdade/Conta.java
+++ Hora da verdade/Conta.java
@@ -1,8 +1,32 @@
 public class Conta {
-   
-   double saldoConta;
 
-   public double consultarSaldoAtual() {
-      return saldoConta;   
-   }
+    double saldoConta;
+    String numero;
+    String nomeCliente;
+    String agencia;
+
+    public double consultarSaldoAtual() {
+
+        return saldoConta;
+    }
+
+    public void sacar(int valor) {
+
+        saldoConta = saldoConta - valor;
+
+    }
+	
+	 
+    public void cancelar(){
+		
+        System.out.println("Conta cancelada");
+		
+    }
+
+    
+    public void transferirValor(String numeroConta, double valor){
+        System.out.println("A conta " + numeroConta + " recebeu uma transferência do valor " + valor)
+
+    } 
+	
 }
